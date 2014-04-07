0778482906c516708f83322cd884325f9d1cccb8
Michael Eickenberg <michael.eickenberg@gmail.com>
2014-04-07 16:01:09 +0200

    glm

--- preparation/glm.py
+++ preparation/glm.py
@@ -37,9 +37,15 @@ def make_paradigm(filename, **kwargs):
                          **kwargs)
 
 
-def do_glm_for_subject(subject_id, bold_base_folder, trial_base_folder):
+def do_glm_for_subject(subject_id, bold_base_folder, trial_base_folder,
+                       output_base_folder):
 
     subject_dir = path(bold_base_folder) / ("sub%03d" % subject_id)
+    output_dir = (path(output_base_folder) / ("sub%03d" % subject_id) /
+                  "model001")
+    if not output_dir.exists():
+        output_dir.make_dirs()
+
 
     task_bold_files = [subject_dir.glob("task001_run%03d/rbold*.nii"
                                         % rid)[0]
@@ -85,10 +91,52 @@ def do_glm_for_subject(subject_id, bold_base_folder, trial_base_folder):
         design_matrices.append(design_matrix)
         all_frametimes.append(frametimes)
 
+        # specify contrasts
+        contrasts = {}
+        n_columns = len(design_matrix.names)
+        for i in xrange(paradigm.n_conditions):
+            contrasts['%s' % design_matrix.names[2 * i]] = np.eye(
+                n_columns)[2 * i]
+
+        # more interesting contrasts"""
+        contrasts['Famous-Unfamiliar'] = contrasts[
+            'Famous'] - contrasts['Unfamiliar']
+        contrasts['Unfamiliar-Famous'] = -contrasts['Famous-Unfamiliar']
+        contrasts['Famous-Scrambled'] = contrasts[
+            'Famous'] - contrasts['Scrambled']
+        contrasts['Scrambled-Famous'] = -contrasts['Famous-Scrambled']
+        contrasts['Unfamiliar-Scrambled'] = contrasts[
+            'Unfamiliar'] - contrasts['Scrambled']
+        contrasts['Scrambled-Unfamiliar'] = -contrasts['Unfamiliar-Scrambled']
+
+        list_of_contrast_dicts.append(contrasts)
+
+
+    # importat maps
+    z_maps = {}
+    effects_maps = {}
+
     fmri_glm = FMRILinearModel(task_bold_files,
                                [dm.matrix for dm in design_matrices],
                                mask="compute")
     fmri_glm.fit(do_scaling=True, model="ar1")
+
+    # replicate contrasts across runs
+    contrasts = dict((cid, [contrasts[cid]
+                            for contrasts in list_of_contrast_dicts])
+                     for cid, cval in contrasts.iteritems())
+
+    # compute effects
+    for contrast_id, contrast_val in contrasts.iteritems():
+        print "\tcontrast id: %s" % contrast_id
+        z_map, eff_map, var_map = fmri_glm.contrast(
+            contrast_val,
+            con_id=contrast_id,
+            output_z=True,
+            output_stat=False,
+            output_effects=True,
+            output_variance=True
+            )
 
     return fmri_glm
 
@@ -104,8 +152,10 @@ if __name__ == "__main__":
 
     bold_base_folder = config["preprocessed"]
     trial_base_folder = config["fmri_raw"]
+    output_base_folder = config["openfmri_dir"]
 
     for sid in subject_ids:
-        res = do_glm_for_subject(sid, bold_base_folder, trial_base_folder)
+        res = do_glm_for_subject(sid, bold_base_folder, trial_base_folder,
+                                 output_base_folder)
 
 
