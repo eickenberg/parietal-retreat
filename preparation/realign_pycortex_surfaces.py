d77ae89b920d1fc0497a051ca2f6e7531f354032
Michael Eickenberg <michael.eickenberg@gmail.com>
2014-04-08 12:59:18 +0200

    realign

---
+++ preparation/realign_pycortex_surfaces.py
@@ -0,0 +1,47 @@
+import json
+config = json.load(open("config.json"))
+
+from path import path
+import os
+import nibabel as nb
+import cortex
+
+
+def realign_surface(subject_id):
+
+    src_subj_name = "sub%03d" % subject_id
+    pycortex_subj_name = "Henson2010_%s" % src_subj_name
+
+    bold_file = (path(config["preprocessed"]) / src_subj_name /
+                 "task001_run001").glob("rbold*.nii")[0]
+
+    mean_bold_file_name = os.tmpnam() + ".nii"
+
+    img = nb.load(bold_file)
+    aff = img.get_affine()
+    mean_data = img.get_data().mean(-1)
+
+    nb.save(nb.Nifti1Image(mean_data, aff), mean_bold_file_name)
+
+    cortex.align.automatic(pycortex_subj_name, 
+                           xfmname="xfm001",
+                           reference=mean_bold_file_name)
+
+    os.remove(mean_bold_file_name)
+
+
+if __name__ == "__main__":
+    SUBJECTS_DIR = config["SUBJECTS_DIR"]
+    import os
+    os.environ["SUBJECTS_DIR"] = SUBJECTS_DIR
+
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("--subject-ids", nargs="+")
+    args = parser.parse_args()
+
+    subject_ids = (args.subject_ids is not None and
+                   [int(sid) for sid in args.subject_ids]) or range(1, 17)
+
+    for subject_id in subject_ids:
+        realign_surface(subject_id)
