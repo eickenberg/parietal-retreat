3d98e7f4a791be2c1b073b50732211bfe2bf15db
Michael Eickenberg <eickenbe@drago.saclay.inria.fr>
2014-04-09 12:38:46 +0200

    ok

--- fmri/z_maps_to_fsaverage.py
+++ fmri/z_maps_to_fsaverage.py
@@ -1,7 +1,7 @@
 from surfer.io import project_volume_data
 from path import path
 from nibabel import gifti
-
+import nibabel as nb
 
 def project_z_map(in_file, subject, hemi, proj_frac, fsaverage_fwhm=3.):
 
@@ -16,21 +16,24 @@ def project_z_map(in_file, subject, hemi, proj_frac, fsaverage_fwhm=3.):
 
     out_file_name = out_dir / (out_base + "-projfrac-%1.2f-%s.gii"
                                % (proj_frac, hemi))
+    print out_file_name
     out_file_fsaverage = out_dir / ("avg_%s-projfrac-%1.2f-%s.gii"
                                     % (out_base, proj_frac, hemi))
-
-    out_data = project_z_maps(in_file, hemi, subject_id=subject,
-                              projmeth="frac", projarg=projfrac)
-    out_data_avg = project_z_maps(in_file, hemi, subject_id=subject,
-                                  proj_meth="frac", projarg=projfrac,
-                                  target_subject="fs_average",
-                                  smooth_fwhm=fsaverage_fwhm)
+    print out_file_fsaverage
+    out_data = project_volume_data(in_file, hemi, subject_id=subject,
+                              projmeth="frac", projarg=proj_frac,
+                                   projsum="point")
+    out_data_avg = project_volume_data(in_file, hemi, subject_id=subject,
+                                  projmeth="frac", projarg=proj_frac,
+                                  target_subject="fsaverage",
+                                  smooth_fwhm=fsaverage_fwhm,
+                                       projsum="point")
 
     gii_arr = gifti.GiftiDataArray.from_array(out_data, 0)
     out_gii = gifti.GiftiImage(darrays=[gii_arr])
 
     gii_arr_avg = gifti.GiftiDataArray.from_array(out_data_avg, 0)
-    out_gii_avg = gifti.GiftiImage(darrays[gii_arr_avg])
+    out_gii_avg = gifti.GiftiImage(darrays=[gii_arr_avg])
 
     gifti.write(out_gii, out_file_name)
     gifti.write(out_gii_avg, out_file_fsaverage)
@@ -49,7 +52,10 @@ if __name__ == "__main__":
                    [int(sid) for sid in args.subject_ids]) or range(1, 17)
 
     hemis = ["lh", "rh"]
-    proj_fracs = [0.5, 0.0, 1.0]
+    proj_fracs = [0.5, 0.1, 0.9]
+
+    import os
+    tmpname = os.tmpnam() + ".nii"
 
     for subject_id in subject_ids:
         openfmri_subj_name = "sub%03d" % subject_id
@@ -62,4 +68,12 @@ if __name__ == "__main__":
                     print fname
                     print hemi
                     print proj_frac
+                    # if fname[-3:] == ".gz":
+                    #     nb.save(nb.load(fname), tmpname)
+                    #     fname2 = tmpname
+                    # else:
+                    #     fname2 = fname
                     project_z_map(fname, fs_subj_name, hemi, proj_frac)
+
+# os.remove(tmpname)
+
